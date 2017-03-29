42493ca2f8157fc60f7f4e6a6bf4160467d001ff
dik-dik <dik-dik@users.noreply.github.com>
2017-03-29 14:59:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 ini_set('display_errors', 1);
-require_once('TwitterAPIExchange.php');
 require_once('common.php');
 
 ?>
@@ -12,6 +11,7 @@ require_once('common.php');
     <style>
         body {
             font-family: "Comic Sans MS", sans-serif;
+			text-align:center;
         }
         
         h2 {
@@ -42,63 +42,27 @@ require_once('common.php');
 </head>
 
 <body>
-    <div align="center" id="whole">
-        <h2 class="nomobile"><br/>"What's the count?"</h2>
+	<h2 class="nomobile"><br/>"What's the count?"</h2>
+<?php
 
+foreach ($arr as $key => $value) {
+	echo "<div class='blok'>
+		<h3><a class='title' href='http://twitter.com/".$key."'>@".$key."</a></h3>"
+		.date_format($value['close'],'l')."<h3><a class='thecount' href='".$value['link']."'>"
+		.$value['count']."</a></h3><div class=nomobile>(".$value['total'].")<br/>&nbsp;</div>
+		<div>Rate: ".round($value['rate'],2)." per hour<br/>"
+		.round(24*$value['rate'],2)." per day
+		<br/>&nbsp;<br/>
+		Pace: ".round($value['pace'],2)."</div>
+		<div class=nomobile>&nbsp;<br/>
+		<b>Remaining: ".$value['remaining']->format("%a d %h h %i m")."</b>
+		<br/>Elapsed: ".$value['elapsed']->format("%a d %h h %i m")."
+		</div></div>";
+}
 
-<div class="blok">
-                    <h3><a class="title" href="http://twitter.com/potus">@POTUS</a></h3><?php echo date_format($potusclose,"l"); ?>
-                    <h3><a class="thecount" href="<?php echo $potuslink; ?>"><?php echo $potuscount; ?></a></h3>
-                    <div class="nomobile">(<?php echo $potustotal; ?>)<br/>&nbsp;</div>          
-                Rate: <?php echo round($potusrate,2) ?> per hour
-            	<br/><?php echo round(24*$potusrate,2) ?> per day<br/>&nbsp;<br/>
-            	Pace: <?php echo round($potustarget,2) ?>
-            	<div class="nomobile">&nbsp;<br/>
-            	<!--Began: <?php echo date_format($potusopen,"Y/m/d H:i:s T"); ?> <br/>
-            	End: <?php echo date_format($potusclose,"Y/m/d H:i:s T"); ?>
-            	<br/>&nbsp;<br/>-->
-            	<b>Remaining: <?php echo $potusinterval->format("%a d %h h %i m"); ?></b>
-            	<br/>
-            	Elapsed: <?php echo $potuselapsed->format("%a d %h h %i m"); ?>
-            	</div>
-</div>
-        
-<div class="blok">
-                    <h3><a class="title" href="http://twitter.com/realDonaldTrump">@realDonaldTrump</a></h3><?php echo date_format($rdtclose,"l"); ?>
-                    <h3><a class="thecount" href="<?php echo $rdtlink; ?>"><?php echo $realDonaldTrumpcount; ?></a></h3>
-                                        <div class="nomobile">(<?php echo $realDonaldTrumptotal; ?>)<br/>&nbsp;</div>
-                <div>Rate: <?php echo round($rdtrate,2) ?> per hour
-            	<br/><?php echo round(24*$rdtrate,2) ?> per day<br/>&nbsp;<br/>
-            	Pace: <?php echo round($rdttarget,2) ?>
-            	<div class="nomobile">&nbsp;<br/>
-            	<!--Began: <?php echo date_format($rdtopen,"Y/m/d H:i:s T"); ?> <br/>
-            	End: <?php echo date_format($rdtclose,"Y/m/d H:i:s T"); ?>
-            	<br/>&nbsp;<br/>-->
-            	<b>Remaining: <?php echo $rdtinterval->format("%a d %h h %i m"); ?></b>
-            	<br/>
-            	Elapsed: <?php echo $rdtelapsed->format("%a d %h h %i m"); ?></div>
-            	</div>
-</div>
+$filename = 'data.txt';
+finishHim($filename,$arr);
+?>
 
-<div class="blok">
-                    <h3><a class="title" href="http://twitter.com/vp">@VP</a></h3><?php echo date_format($vpclose,"l"); ?>
-                    <h3><a class="thecount" href="<?php echo $vplink; ?>"><?php echo $vpcount; ?></a></h3>
-                    <div class="nomobile">(<?php echo $vptotal; ?>)<br/>&nbsp;</div>
-            	Rate: <?php echo round($vprate,2) ?> per hour
-            	<br/><?php echo round(24*$vprate,2) ?> per day<br/>&nbsp;<br/>
-            	Pace: <?php echo round($vptarget,2) ?>
-            	<div class="nomobile">&nbsp;<br/>
-            	<!--Began: <?php echo date_format($vpopen,"Y/m/d H:i:s T"); ?> <br/>
-            	End: <?php echo date_format($vpclose,"Y/m/d H:i:s T"); ?>
-            	<br/>&nbsp;<br/>-->
-            	<b>Remaining: <?php echo $vpinterval->format("%a d %h h %i m"); ?></b>
-            	<br/>
-            	Elapsed: <?php echo $vpelapsed->format("%a d %h h %i m"); ?>
-            	</div>
-</div>
-
-     
-    </div>
 </body>
-
 </html>
