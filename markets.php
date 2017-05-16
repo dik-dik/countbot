a8ce84d2baa06bba9ee02df0627318e5d1b72785
dik-dik <dik-dik@users.noreply.github.com>
2017-05-16 18:25:55 -0500

    Update markets.php

--- markets.php
+++ markets.php
@@ -1,40 +1,31 @@
 <?php
 ini_set('display_errors', 1);
 
-//Grab Market Data
+$file = 'cache/' . basename($_SERVER['SCRIPT_NAME']); //location of cache file
+$current_time = time();
+$cache_last_modified = filemtime($file); //time when the cache file was last modified
 
+if(file_exists($file) && ($current_time < strtotime('+15 minute', $cache_last_modified))){ //check if cache file exists and hasn't expired yet
+  echo "Last updated ".date("F d, Y H:i:s T",$cache_last_modified);
+  include($file); //include cache file
+}else{
+  ob_start(); //start output buffering
+
+
+//Grab Market Data
 $pi_json_file = file_get_contents('https://www.predictit.org/api/marketdata/all');
 $pi_data = json_decode($pi_json_file);
 
 ?>
-<html>
 
-<head>
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<html><head><script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">  
     <title>All PI Markets</title>
 	
 	<link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.10.13/css/jquery.dataTables.css">
 	
-    <style>
-        body {
-            font-family: sans-serif;
-        }
-        
-        a { text-decoration: none;}
-        a.link, a.visited { color: #4286f4; }          
-        a:hover { color: #a8088d; }       
-        
-        .nomobile { display: none; }
-        
-        @media screen and (min-width: 768px) {
-    		.nomobile {
-		        clear: both;
-		        display: block;
-    		}
-	}
-    </style>
+    <style>body{font-family:sans-serif}a{text-decoration:none}a.link,a.visited{color:#4286f4}a:hover{color:#a8088d}</style>
 
 	<script type="text/javascript" charset="utf8" src="http://cdn.datatables.net/1.10.13/js/jquery.dataTables.js"></script>
 	<script type="text/javascript">
@@ -45,59 +36,30 @@ $pi_data = json_decode($pi_json_file);
 	});</script>
 	
 </head>
-
-<body>
-<table  id="table_id" class="display">
-<thead>
-<tr>
-<th>Market</th>
-<th>Contract</th>
-<th>Latest</th>
-<th>Change</th>
-<th>Buy Yes</th>
-<th>Sell Yes</th>
-<th>Buy No</th>
-<th>Sell No</th>
-<th>End Date</th>
-</tr>
-</thead>
-<tbody>
+<body><table  id="table_id" class="display"><thead><tr><th>Market</th><th>Contract</th><th>Latest</th><th>Change</th><th>Buy Yes</th><th>Sell Yes</th><th>Buy No</th><th>Sell No</th><th>End Date</th></tr></thead><tbody>
 <?php
 
 foreach($pi_data->Markets as $item)
 {
 	foreach ($item->Contracts as $subitem)
-		{	echo "<tr><td><a href=";
-			echo $item->URL;
-			echo ">";
-			echo $item->ShortName;
-			echo "</a></td>\n<td><a href=";
-			echo $subitem->URL;
-			echo ">";
-			echo $subitem->Name;
-			echo "</a></td>\n<td class='dt-right'>";
-			echo number_format ($subitem->LastTradePrice, 2);
-			echo "</td>\n<td class='dt-right'>";
+		{	echo "<tr><td><a href=".$item->URL.">".$item->ShortName."</a></td><td><a href=".$subitem->URL.">".$subitem->Name."</a></td><td class='dt-right'>".number_format ($subitem->LastTradePrice, 2)."</td><td class='dt-right'>";
 			if ($subitem->LastClosePrice) {echo number_format($subitem->LastTradePrice - $subitem->LastClosePrice, 2);} else {echo 0.00;}
-			echo "</td>\n<td class='dt-right'>";
-			echo number_format($subitem->BestBuyYesCost, 2);
-			echo "</td>\n<td class='dt-right'>";
-			echo number_format($subitem->BestSellYesCost, 2);
-			echo "</td>\n<td class='dt-right'>";
-			echo number_format($subitem->BestBuyNoCost, 2);
-			echo "</td>\n<td class='dt-right'>";
-			echo number_format($subitem->BestSellNoCost, 2);
-			echo "</td>\n<td>";
-			echo $subitem->DateEnd;
-			echo "</td></tr>\n";
+			echo "</td><td class='dt-right'>".number_format($subitem->BestBuyYesCost, 2)."</td><td class='dt-right'>".number_format($subitem->BestSellYesCost, 2)."</td><td class='dt-right'>".number_format($subitem->BestBuyNoCost, 2)."</td><td class='dt-right'>".number_format($subitem->BestSellNoCost, 2)."</td><td>".$subitem->DateEnd."</td></tr>";
 		}
 	echo "</tr>";
 }
+
+?>
+</tbody></table></body></html>
+
+<?php
+	
+
+  $fp = fopen($file, 'w'); //open cache file
+  fwrite($fp, ob_get_contents()); //create new cache file
+  fclose($fp); //close cache file
+  ob_end_flush(); //flush output buffered
+}
+	
 	
 ?>
-</tbody>
-</table>
-
-</body>
-
-</html>
