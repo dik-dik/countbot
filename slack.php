d3fa04f874f34d72e46a73f8ea347ece4bde761a
dik-dik <dik-dik@users.noreply.github.com>
2017-05-16 18:26:55 -0500

    Update slack.php

--- slack.php
+++ slack.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 require_once('common.php');
 
 $token = $_POST['token'];
+$text = $_POST['text'];
+
 
 if($token != $settings['token']){
   $msg = "The token for the slash command doesn't match. Check your script.";
@@ -14,7 +16,19 @@ if($token != $settings['token']){
 $reply ="";
 
 foreach ($arr as $key => $value) {
-	$reply = $reply."<".$value['link']."|*".$key."*> ".$value['count']."       ";
+	$reply = $reply."<".$value['link']."|*".$key."*> ".$value['count']."        ";
+}
+
+if ($text){
+
+	$reply = "_counts:_ ".$reply."\n_".$text.":_   ";
+	foreach ($arr as $key => $value) {
+		$value['pace'] = round($value['pace'],2);
+		$value['close'] = date_format($value['close'],'l');
+		$value['remaining'] = $value['remaining']->format("%a d %h h %i m");
+		$value['elapsed'] = $value['elapsed']->format("%a d %h h %i m");
+		$reply = $reply."<".$value['link']."|*".$key."*> ".$value[$text]."   ";
+	}
 }
 
 $reply = $reply."<http://realcount.club/|more>";
